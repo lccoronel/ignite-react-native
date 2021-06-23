5da00236259e8eec93ab05cdafe23eb2570f90b3
Lucas Cardoso <54275445+lccoronel@users.noreply.github.com>
2021-06-23 19:18:15 -0300

    feat: styled components setup

---
+++ react-native/gofinances/src/screen/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const screen: React.FC = () => {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
+}
+
+export default screen;
