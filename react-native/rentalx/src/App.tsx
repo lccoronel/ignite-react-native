90e0538eaf30b695d50d7d249c2b71cd92034319
Lucas Cardoso <54275445+lccoronel@users.noreply.github.com>
2021-08-13 16:52:24 -0300

    feat: load fonts

--- react-native/rentalx/src/App.tsx
+++ react-native/rentalx/src/App.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import {
+  useFonts,
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import AppLoading from 'expo-app-loading';
+
 import Home from './screens/Home';
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return <Home />;
 };
 
