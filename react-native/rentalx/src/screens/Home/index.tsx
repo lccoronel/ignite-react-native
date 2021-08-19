4a5120c77fd75d6d3ed6005ba1562f79cfa92f52
Lucas Coronel <lccoronel7@icloud.com>
2021-08-19 18:27:17 -0300

    feat: setuo svg extension

--- react-native/rentalx/src/screens/Home/index.tsx
+++ react-native/rentalx/src/screens/Home/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
+import Logo from '../../assets/logo.svg';
 import { Container, Header } from './styles';
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <Header />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <Logo />
+      </Header>
     </Container>
   );
 };
