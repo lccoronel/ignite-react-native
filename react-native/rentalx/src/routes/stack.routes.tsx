e8e73268b7b3e6c313fb3433850be5dbf38acf71
Lucas Coronel <lccoronel7@icloud.com>
2021-11-03 07:50:26 -0300

    feat: reanimated lib

--- react-native/rentalx/src/routes/stack.routes.tsx
+++ react-native/rentalx/src/routes/stack.routes.tsx
@@ -7,12 +7,14 @@ import Schedulling from '../screens/Schedulling';
 import SchedullingDetails from '../screens/SchedullingDetails';
 import SchedullingComplete from '../screens/SchedullingComplete';
 import Mycars from '../screens/Mycars';
+import Splash from '../screens/Splash';
 
 const { Navigator, Screen } = createStackNavigator();
 
 const StackRoutes: React.FC = () => {
   return (
-    <Navigator headerMode="none">
+    <Navigator headerMode="none" initialRouteName="Splash">
+      <Screen name="Splash" component={Splash} />
       <Screen name="Home" component={Home} />
       <Screen name="CarDetails" component={CarDetails} />
       <Screen name="Schedulling" component={Schedulling} />
