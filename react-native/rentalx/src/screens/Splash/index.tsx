d7a5ebfeaeeaaba840ea888d26f6f9088ae54666
Lucas Coronel <lccoronel7@icloud.com>
2021-11-05 07:51:42 -0300

    feat: first animation

--- react-native/rentalx/src/screens/Splash/index.tsx
+++ react-native/rentalx/src/screens/Splash/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Button, StyleSheet } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 
 import { Container } from './styles';
 
 const Splash: React.FC = () => {
-  return <Container />;
+  const animation = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: animation.value }],
+    };
+  });
+
+  function handleAnimationPosition() {
+    animation.value = Math.random() * 500;
+  }
+
+  return (
+    <Container>
+      <Animated.View style={[styles.box, animatedStyles]} />
+
+      <Button title="Mover" onPress={handleAnimationPosition} />
+    </Container>
+  );
 };
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  },
+});
 
 export default Splash;
