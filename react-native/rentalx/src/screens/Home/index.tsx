b7e714c163d2e43b43cecf7f9c6eea739659078e
Lucas Coronel <lccoronel7@icloud.com>
2021-11-23 10:03:49 -0300

    feat: animation button my cars

--- react-native/rentalx/src/screens/Home/index.tsx
+++ react-native/rentalx/src/screens/Home/index.tsx
@@ -4,6 +4,13 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import { useNavigation } from '@react-navigation/native';
 import { useTheme } from 'styled-components';
 import { Ionicons } from '@expo/vector-icons';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+  withSpring,
+} from 'react-native-reanimated';
+import { PanGestureHandler } from 'react-native-gesture-handler';
 
 import Logo from '../../assets/logo.svg';
 import Car from '../../components/Car';
@@ -18,6 +25,33 @@ const Home: React.FC = () => {
   const [cars, setCars] = useState<ICarDTO[]>([]);
   const [loading, setLoading] = useState(true);
 
+  const positionY = useSharedValue(0);
+  const positionX = useSharedValue(0);
+
+  const myCarsButtonStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: positionX.value },
+        { translateY: positionY.value },
+      ],
+    };
+  });
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart(_, context: any) {
+      context.positionX = positionX.value;
+      context.positionY = positionY.value;
+    },
+    onActive(event, context: any) {
+      positionX.value = context.positionX + event.translationX;
+      positionY.value = context.positionY + event.translationY;
+    },
+    onEnd() {
+      positionX.value = withSpring(0);
+      positionY.value = withSpring(0);
+    },
+  });
+
   useEffect(() => {
     try {
       api.get('cars').then(response => setCars(response.data));
@@ -27,10 +61,6 @@ const Home: React.FC = () => {
       setLoading(false);
     }
   }, []);
-
-  function navigateToCarDetail(car: ICarDTO) {
-    navigate('CarDetails', { car });
-  }
 
   return (
     <Container>
@@ -53,14 +83,26 @@ const Home: React.FC = () => {
           data={cars}
           keyExtractor={item => item.id}
           renderItem={({ item }) => (
-            <Car data={item} onPress={() => navigateToCarDetail(item)} />
+            <Car
+              data={item}
+              onPress={() => navigate('CarDetails', { car: item })}
+            />
           )}
         />
       )}
 
-      <MyCarsButton onPress={() => navigate('MyCars')}>
-        <Ionicons name="ios-car-sport" color={colors.shape} size={32} />
-      </MyCarsButton>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View
+          style={[
+            myCarsButtonStyle,
+            { position: 'absolute', bottom: 13, right: 22 },
+          ]}
+        >
+          <MyCarsButton onPress={() => navigate('MyCars')}>
+            <Ionicons name="ios-car-sport" color={colors.shape} size={32} />
+          </MyCarsButton>
+        </Animated.View>
+      </PanGestureHandler>
     </Container>
   );
 };
