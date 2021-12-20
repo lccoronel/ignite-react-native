597c95de3076bb3b3f0b68aad433d41fe2664d62
Lucas Coronel <lccoronel7@icloud.com>
2021-12-20 10:07:17 -0300

    🎨  add new max-len rule in eslint

--- react-native/rentalx/src/screens/Signin/index.tsx
+++ react-native/rentalx/src/screens/Signin/index.tsx
@@ -1,32 +1,41 @@
 import React, { useState } from 'react';
-import {
-  StatusBar,
-  KeyboardAvoidingView,
-  TouchableWithoutFeedback,
-  Keyboard,
-} from 'react-native';
+import { StatusBar, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import { useTheme } from 'styled-components';
+import * as Yup from 'yup';
 
 import Button from '../../components/Button';
 import { Input } from '../../components/Input';
 import { PasswrodInput } from '../../components/PasswordInput';
 import { Container, Header, Title, SubTitle, Footer, Form } from './styles';
 
-const Signin: React.FC = () => {
+export const Signin: React.FC = () => {
   const { colors } = useTheme();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  async function handleSignin() {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string().required('E-mail obrigstorio').email('E-mail invalido'),
+        password: Yup.string().required('Senha obrigatoria'),
+      });
+
+      await schema.validate({ email, password });
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        Alert.alert('Erro de autenticação', err.message);
+      } else {
+        console.log('outro erro');
+      }
+    }
+  }
+
   return (
     <KeyboardAvoidingView behavior="position" enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <Container>
-          <StatusBar
-            barStyle="dark-content"
-            backgroundColor="transparent"
-            translucent
-          />
+          <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
 
           <Header>
             <Title>
@@ -50,21 +59,11 @@ const Signin: React.FC = () => {
               onChangeText={setEmail}
             />
 
-            <PasswrodInput
-              iconName="lock"
-              placeholder="Senha"
-              value={password}
-              onChangeText={setPassword}
-            />
+            <PasswrodInput iconName="lock" placeholder="Senha" value={password} onChangeText={setPassword} />
           </Form>
 
           <Footer>
-            <Button
-              title="Login"
-              onPress={() => console.log()}
-              enabled
-              loading={false}
-            />
+            <Button title="Login" onPress={handleSignin} enabled loading={false} />
 
             <Button
               light
@@ -80,5 +79,3 @@ const Signin: React.FC = () => {
     </KeyboardAvoidingView>
   );
 };
-
-export default Signin;
