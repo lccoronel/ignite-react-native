16ba9c20aa664191e7491efe8c27a5b6647406a6
Lucas Coronel <lccoronel7@icloud.com>
2021-12-23 08:06:08 -0300

    ➕ watermelondb setup ios

---
+++ react-native/rentalx/ios/rentalx-Bridging-Header.h
@@ -0,0 +1,6 @@
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import <React/RCTViewManager.h>
+#import <React/RCTBridgeModule.h>
+
+#import "../../node_modules/@nozbe/watermelondb/native/ios/WatermelonDB/SupportingFiles/Bridging.h"
---
+++ react-native/rentalx/ios/wmelon.swift
@@ -0,0 +1,8 @@
+//
+//  wmelon.swift
+//  rentalx
+//
+//  Created by Lucas Coronel on 23/12/21.
+//
+
+import Foundation
