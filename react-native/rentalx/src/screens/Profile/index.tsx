ac4a1b360a6109aef6915f8eb3d1427395b24cf0
Lucas Coronel <lccoronel7@icloud.com>
2021-12-31 18:06:11 -0300

    🚧 profile screen

--- react-native/rentalx/src/screens/Profile/index.tsx
+++ react-native/rentalx/src/screens/Profile/index.tsx
@@ -1,34 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Keyboard, KeyboardAvoidingView, TouchableWithoutFeedback } from 'react-native';
 import { useTheme } from 'styled-components';
 import { useNavigation } from '@react-navigation/native';
-
 import { Feather } from '@expo/vector-icons';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+
 import BackButton from '../../components/BackButton';
-import { Container, Header, HeaderTop, HeaderTitle, LogountButton, PhotoContainer, Photo, PhotoButton } from './styles';
+import { Input } from '../../components/Input';
+import { PasswordInput } from '../../components/PasswordInput';
+import { useAuth } from '../../hook/auth';
+import {
+  Container,
+  Header,
+  HeaderTop,
+  HeaderTitle,
+  LogountButton,
+  PhotoContainer,
+  Photo,
+  PhotoButton,
+  Content,
+  Options,
+  Option,
+  OptionTitle,
+  Section,
+} from './styles';
 
 export const Profile: React.FC = () => {
   const { colors } = useTheme();
   const { goBack } = useNavigation();
+  const { user } = useAuth();
+
+  const [option, setOption] = useState<'dataEdit' | 'passwordEdit'>('dataEdit');
 
   return (
-    <Container>
-      <Header>
-        <HeaderTop>
-          <BackButton color={colors.shape} onPress={goBack} />
-          <HeaderTitle>Editar perfil</HeaderTitle>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <HeaderTop>
+              <BackButton color={colors.shape} onPress={goBack} />
+              <HeaderTitle>Editar perfil</HeaderTitle>
 
-          <LogountButton>
-            <Feather name="power" size={24} color={colors.shape} />
-          </LogountButton>
-        </HeaderTop>
+              <LogountButton>
+                <Feather name="power" size={24} color={colors.shape} />
+              </LogountButton>
+            </HeaderTop>
 
-        <PhotoContainer>
-          <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/54275445?v=4' }} />
-          <PhotoButton>
-            <Feather name="camera" size={24} color={colors.shape} />
-          </PhotoButton>
-        </PhotoContainer>
-      </Header>
-    </Container>
+            <PhotoContainer>
+              <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/54275445?v=4' }} />
+              <PhotoButton>
+                <Feather name="camera" size={24} color={colors.shape} />
+              </PhotoButton>
+            </PhotoContainer>
+          </Header>
+
+          <Content style={{ marginBottom: useBottomTabBarHeight() }}>
+            <Options>
+              <Option active={option === 'dataEdit'} onPress={() => setOption('dataEdit')}>
+                <OptionTitle active={option === 'dataEdit'}>Dados</OptionTitle>
+              </Option>
+
+              <Option active={option === 'passwordEdit'} onPress={() => setOption('passwordEdit')}>
+                <OptionTitle active={option === 'passwordEdit'}>Trocar senha</OptionTitle>
+              </Option>
+            </Options>
+
+            {option === 'dataEdit' ? (
+              <Section>
+                <Input iconName="user" placeholder="Nome" autoCorrect={false} defaultValue={user.name} />
+                <Input iconName="mail" editable={false} defaultValue={user.email} />
+                <Input
+                  iconName="credit-card"
+                  placeholder="CNH"
+                  keyboardType="numeric"
+                  defaultValue={user.driver_license}
+                />
+              </Section>
+            ) : (
+              <Section>
+                <PasswordInput iconName="lock" placeholder="Senha atual" />
+                <PasswordInput iconName="lock" placeholder="Nova senha" />
+                <PasswordInput iconName="lock" placeholder="Repetir senha" />
+              </Section>
+            )}
+          </Content>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
