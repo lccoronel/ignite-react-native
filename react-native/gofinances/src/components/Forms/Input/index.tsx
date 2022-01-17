48195d7a92120f940d5c50fe6c4c593115e67c8c
Lucas Coronel <lccoronel7@icloud.com>
2022-01-17 07:06:13 -0300

    ✅ test input component

--- react-native/gofinances/src/components/Forms/Input/index.tsx
+++ react-native/gofinances/src/components/Forms/Input/index.tsx
@@ -3,8 +3,10 @@ import { TextInputProps } from 'react-native';
 
 import { Container } from './styles';
 
-type InputProps = TextInputProps;
-
-export const Input: React.FC<InputProps> = ({...rest}) => {
-  return <Container {...rest} />;
+interface InputProps extends TextInputProps {
+  active?: boolean;
 }
+
+export const Input: React.FC<InputProps> = ({ active = false, ...rest }) => {
+  return <Container active={active} {...rest} />;
+};
